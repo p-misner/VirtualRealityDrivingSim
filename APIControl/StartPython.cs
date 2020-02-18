eae23fe5705a9400c662ea21c741a022539e76eb
sbentl02 <58706533+sbentl02@users.noreply.github.com>
2020-02-18 16:34:53 -0500

    Add files via upload

---
+++ APIControl/StartPython.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartPython : MonoBehaviour
+{
+    public string file_address;
+ 
+    void OnMouseDown()
+    {
+        // using System.Diagnostics;
+        ProcessStartInfo p = new ProcessStartInfo();
+        p.FileName = "python";
+        p.Arguments = file_address;
+        p.CreateNoWindow = true;
+        p.RedirectStandardOutput = true;
+
+        // Where the script lives
+        p.UseShellExecute = false;
+        Process process = Process.Start(p);
+
+    }
+
+
+
+
+
+
+}
