2f22f1dde17e010e30ddda825397205bd315b9a0
sbentl02 <58706533+sbentl02@users.noreply.github.com>
2020-02-18 16:06:34 -0500

    Add files via upload

---
+++ Weather/StartFog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartFog : MonoBehaviour
+{
+   
+    public ParticleSystem Fog;
+    // Start is called before the first frame update
+
+    void OnTriggerEnter(Collider other)
+    {
+        Fog.Play();
+
+    }
+}
