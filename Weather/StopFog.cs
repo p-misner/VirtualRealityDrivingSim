f75710b4dabcdab815d54061ac4689f71fb8164b
sbentl02 <58706533+sbentl02@users.noreply.github.com>
2020-02-21 12:37:40 -0500

    Add files via upload

---
+++ Weather/StopFog.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class StopFog : MonoBehaviour
+{
+
+    public ParticleSystem Fog;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Fog.Stop(); //Stops emission, so fog gradually dissipates
+        Fog.Clear(); //Clears all particles, so fog abruptly disappears
+
+    }
+}
