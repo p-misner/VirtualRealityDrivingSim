e4b2530d780aefa0491a819ee500fd334b7f9757
sbentl02 <58706533+sbentl02@users.noreply.github.com>
2020-02-21 11:27:41 -0500

    Update StartFog.cs

--- Weather/StartFog.cs
+++ Weather/StartFog.cs
@@ -1,12 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StartFog : MonoBehaviour
 {
    
     public ParticleSystem Fog;
-    // Start is called before the first frame update
 
     void OnTriggerEnter(Collider other)
     {
