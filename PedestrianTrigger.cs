391e2c2358206a1cb4c4049b118cbac125595089
jyeung9 <44103932+jyeung9@users.noreply.github.com>
2020-03-09 17:24:07 -0400

    pedestrian, leading car scripts

---
+++ PedestrianTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class PedestrianTrigger : MonoBehaviour
+{
+    //public GameObject[] test;
+    public GameObject pedestrian;
+    public NavMeshAgent agent;
+    public Transform destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        var cubeRender = this.GetComponent<Renderer>();
+        Color purple = new Color(0.33f, 0.15f, 0.43f);
+        cubeRender.material.SetColor("_Color",purple);
+        //pedestrian = GameObject.FindGameObjectWithTag("pedestrian");
+        if (pedestrian == null)
+        {
+            Debug.Log("no peds found");
+        }
+        agent.speed = 0.0f;
+
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        pedestrian.gameObject.SetActive(true);
+        agent.destination = destination.position;
+        agent.speed = 1.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PedestrianWalk.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PedestrianWalk : MonoBehaviour
+{
+    // Start is called before the first frame update
+   //public GameObject pedestrian;
+   // public NavMeshAgent agent;
+    public Transform destination;
+    /* void Start()
+     {
+         pedestrian = GameObject.FindGameObjectWithTag("pedestrian");
+         if(pedestrian == null)
+         {
+             Debug.Log("no peds found");
+         }
+         else
+         {
+             Debug.Log(pedestrian.transform.position);
+         }
+         //pedestrian.gameObject.SetActive(false);
+         agent.speed = 0.0f;
+
+        // agent = pedestrian.GetComponent<NavMeshAgent>();
+         agent.SetDestination(destination.position);
+
+     }
+
+     // Update is called once per frame
+     void OnTriggerEnter(Collider other)
+     {
+         pedestrian = GameObject.FindGameObjectWithTag("pedestrian");
+         pedestrian.gameObject.SetActive(true);
+         agent.speed = 1.0f;
+
+     } */
+
+    private void Update()
+    {
+        bool didEnter = TriggerEntered.entered;
+        if (didEnter == true){
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            agent.destination = destination.position;
+            agent.speed = 1.0f;
+        }
+        
+    }
+}
